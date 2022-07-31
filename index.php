57b1de6c0e53988287e4bce9ac3e12a9fd80550c
Nikita <33207529+CodeSquirrelPL@users.noreply.github.com>
2022-07-31 17:12:40 +0200

    rewrote methods to work with the keypad array

--- index.php
+++ index.php
@@ -3,13 +3,8 @@
     <script src="index.js"></script>
 </head>
 
-
 <?php
-
 class PhoneKeyboardConverter {
-
-    public $string = '';
-    public $numeric = '';
 
     public $keypad = [
         '2' => ['a', 'b', 'c'],
@@ -22,25 +17,33 @@ class PhoneKeyboardConverter {
         '9' => ['w', 'x', 'y', 'z']
     ];
 
-    function find($character) {
+    function whichKey($character) {
         foreach($this->keypad as $index => $key) {
-            $pushes = array_search($character, $key);
-            if ($pushes) return [$index, $pushes + 1];
+            $pressedTimes = array_search($character, $key);
+            if ($pressedTimes!==false) {
+                $output = "";
+                $pressedTimes+=1;
+                for ($i=0; $i<$pressedTimes; $i++) {
+                    $output.=$index;
+                }
+                return $output;
+            }
         }
         return false;
     }
 
+    function whichCharacter($number) {
+        $presses = strlen($number);
+        $pressedKey = $number[0];
+        return $this->keypad[$pressedKey][$presses-1];
+    }
+
     function convertToNumeric($input) {
         $output = "";
-        $input = str_split($input);
-        foreach ($input as $character) {
-            //znaki diakrytyczne - zamień na zwykłe; ->lowercase
-             $charNumber = ord($character)-91;
-             $keyNumber = intdiv($charNumber, 3);
-             for ($i = 0; $i<$charNumber%3+1; $i++) {
-                $output.=$keyNumber;
-             }
-             $output.=",";
+        $inputLowercase = strtolower($input);
+        $inputLowercaseArr = str_split($inputLowercase);
+        foreach ($inputLowercaseArr as $character) {
+            echo($this->whichKey($character));
          }
          return $output;
      }
@@ -49,39 +52,22 @@ class PhoneKeyboardConverter {
     function convertToString($input) {
         $output = "";
         $numbers = explode(",", $input);
-        foreach ($numbers as $character) {
-            //czy wszystkie cyfry są takie same, czy są wyłącznie cyfry?, czy jest ich >0 i <4 -> error: incorrect input
-            $pushes = strlen($character);
-            $keyNumber = $character[0]; //można zrobić w walidacji
-            $charNumber = $keyNumber*3+90+$pushes;
-            //$output.=chr($charNumber);
-            //$output.=$charNumber;
-            //$output.=",";
-            $output=$output.'pushes: '.$pushes.' pressed key: '.$keyNumber.' character number: '.$charNumber.' character: '.chr($charNumber).'<br>';
+        foreach ($numbers as $keyPressed) {
+            $output.=$this->whichCharacter($keyPressed);
         }
         return $output;
     }
-
-    function validateKeyNumber($KeyNumberArr) {
-        $keyNumber = array_values($KeyNumberArr);
-        if ($keyNumber) {
-            
-        } 
-    }
-
 };
 
 
 
 
 $test = new PhoneKeyboardConverter();
-//echo($test->convertToString("5,2,22,555,33,222,9999,66,444,55"));
-echo(implode(',', $test->find('ź')));
+//echo($test->convertToString("55,222"));
+$test->convertToNumeric('e');
 ?>
 
-
+<html>
 
 Klawiatury starych telefonów komórkowych były wyłącznie numeryczne. Aby napisać za ich pomocą tekst należało naciskać dany klawisz numeryczny odpowiednią ilość razy, na przykład „ULA” zapisywało się jako 885552 (dwa naciśnięcia klawisza 8, trzy naciśnięcia klawisza 5, jedno naciśnięcia klawisza 2). Stwórz klasę PhoneKeyboardConverter zawierającą metody: 1. convertToNumeric, umożliwiającą tłumaczenie łańcucha znaków na ciąg numeryczny zgodny z klawiaturą telefonu, kolejne „litery” oddzielone powinny być przecinkiem, 2. convertToString , umożliwiającą tłumaczenie ciągu numerycznego zgodnego z klawiaturą telefonu (gdzie kolejne „litery” oddzielone będą przecinkiem) na tekst.
 Metody powinny przyjmować jako wymagany parametr wejściowy ciąg znaków i zwracać przetłumaczony ciąg. Klasa może zawierać dowolne inne zmienne/metody, które zostały uznane za niezbędne bądź pomocne przy wykonaniu zadania.
-
-<html>
